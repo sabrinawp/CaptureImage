d46eaf3ed9ad09c35a3287e58e1e3820ab6318a4
sabrinawp <74299838+sabrinawp@users.noreply.github.com>
2022-11-16 22:20:37 +0700

    Add files via upload

---
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -0,0 +1,35 @@
+﻿Imports AForge
+Imports AForge.Video
+Imports AForge.Video.DirectShow
+Imports System.IO
+Public Class Form1
+    Dim CAMERA As VideoCaptureDevice
+    Dim bmp As Bitmap
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim cameras As VideoCaptureDeviceForm = New VideoCaptureDeviceForm
+        If cameras.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            CAMERA = cameras.VideoDevice
+            AddHandler CAMERA.NewFrame, New NewFrameEventHandler(AddressOf Captured)
+            CAMERA.Start()
+        End If
+    End Sub
+    Private Sub Captured(sender As Object, eventArgs As NewFrameEventArgs)
+        bmp = DirectCast(eventArgs.Frame.Clone(), Bitmap)
+        PictureBox1.Image = DirectCast(eventArgs.Frame.Clone(), Bitmap)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        SaveFileDialog1.DefaultExt = ".jpg"
+        If SaveFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            PictureBox2.Image.Save(SaveFileDialog1.FileName, Imaging.ImageFormat.Jpeg)
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        PictureBox2.Image = PictureBox1.Image
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        CAMERA.Stop()
+    End Sub
+End Class
